c0fc0435c8f4fd6186bf07d75798b6c5ccf6d5d3
c0stra <ondrej.fischer@gmail.com>
2024-08-22 22:27:40 +0200

    Initiated js unit testing.

--- src/main/resources/static/js/ui.js
+++ src/main/resources/static/js/ui.js
@@ -1,7 +1,40 @@
+/*
+ * Copyright (c) 2024-2024 Ondrej Fischer
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 import {addTo, attach, ctrlKey, each, falseTo, node, render, restParameter, set, state, stateModel, to, toggle, transform, trigger, when} from "./rap.js";
 import {a, captionBottom, captionTop, checkbox, div, form, HtmlBuilder, iframe, input, inputText, label, reset, span, submit, table, tbody, td, th, thead, tr} from "./html.js";
 import {get, isChannel} from "./io.js"
 
+/**
+ * UI control toggling a boolean model, used typically for expansion / collapsing of some content.
+ *
+ * @param {object} model - The object representing the element.
+ * @param {boolean} [enabled=true] - Indicates whether the element is enabled or disabled. Default is true.
+ * @returns {object} - The modified element.
+ */
 export function expander(model, enabled = stateModel(true)) {
     return span()
         .display('inline-block')
@@ -14,14 +47,40 @@ export function expander(model, enabled = stateModel(true)) {
 }
 
 export class Column {
-    constructor(name, renderCell) {this.__name = name; this.__hidden = false; if(renderCell) this.renderCell = renderCell; }
-    getName() {return this.__name}
-    getValue(data) {return data?.[this.getName()]}
-    renderHeader(th, columnPosition, table) {return this.getName()}
-    renderCell(data, td, rowIndex, table) {return this.getValue(data)}
-    hidden() {return this.__hidden}
-    hide(value = true) {this.__hidden = value; return this}
-    show(value = true) {return this.hide(!value)}
+    constructor(name, renderCell) {
+        this.__name = name;
+        this.__hidden = false;
+        if (renderCell) this.renderCell = renderCell;
+    }
+
+    getName() {
+        return this.__name
+    }
+
+    getValue(data) {
+        return data?.[this.getName()]
+    }
+
+    renderHeader(th, columnPosition, table) {
+        return this.getName()
+    }
+
+    renderCell(data, td, rowIndex, table) {
+        return this.getValue(data)
+    }
+
+    hidden() {
+        return this.__hidden
+    }
+
+    hide(value = true) {
+        this.__hidden = value;
+        return this
+    }
+
+    show(value = true) {
+        return this.hide(!value)
+    }
 }
 
 export let position = new Column('#', (data, td, index) => index)
@@ -47,7 +106,7 @@ function detectSource(model) {
 
 function embeddedSource(data) {
     let listEntries = Object.entries(data).filter(([key]) => key.endsWith('List'))
-    if(listEntries.length === 1) return listEntries[0][1]
+    if (listEntries.length === 1) return listEntries[0][1]
     return []
 }
 
@@ -61,6 +120,10 @@ function detectSearch(model) {
     return transform(model, data => data?._links?.search)
 }
 
+/**
+ * Represents a data table element that can render data based on the provided data model and column definitions.
+ * @extends HtmlBuilder
+ */
 export class DataTable extends HtmlBuilder {
 
     constructor(dataModel, t = table()) {
@@ -74,7 +137,7 @@ export class DataTable extends HtmlBuilder {
         this.add(
             thead(tr(each(this.visibleColumnsModel, (column, index) => {
                 let header = th().resizeHorizontal().overflowHidden()
-                if(this.moveEnabled) header
+                if (this.moveEnabled) header
                     .transfer(columnMove, index)
                     .receive(columnMove, from => this.moveColumn(from, index), 'rap-table-header-receiver', 'rap-table-header-drop')
                 return header.add(column.renderHeader(header, index, this))
@@ -86,9 +149,15 @@ export class DataTable extends HtmlBuilder {
         )
     }
 
-    repaint() {this.columnsModel.trigger(); return this}
+    repaint() {
+        this.columnsModel.trigger();
+        return this
+    }
 
-    enableColumnMove() {this.moveEnabled = true; return this.repaint()}
+    enableColumnMove() {
+        this.moveEnabled = true;
+        return this.repaint()
+    }
 
     enableColumnFiltering() {
         let vis = state(false)
@@ -98,7 +167,10 @@ export class DataTable extends HtmlBuilder {
                     a('⋮').setClass('rap-columns-toggle').onClick(toggle(vis)),
                     div().setClass('rap-columns-visibility').display(vis).position('absolute').textLeft().whiteSpace('nowrap').right(0).add(
                         each(this.columnsModel, column => div().add(
-                            checkbox(column.getName()).checked(column.hidden() ? null : 'checked').id(column.getName()).onChange(() => {column.hide(!column.hidden()); this.columnsModel.trigger()}, true),
+                            checkbox(column.getName()).checked(column.hidden() ? null : 'checked').id(column.getName()).onChange(() => {
+                                column.hide(!column.hidden());
+                                this.columnsModel.trigger()
+                            }, true),
                             label(column.getName()).set('for', column.getName())
                         ))
                     )
@@ -115,7 +187,7 @@ export class DataTable extends HtmlBuilder {
     transferItems(slot) {
         return this.customizeRow((tr, data) => tr.transfer(slot, data))
     }
-    
+
     customizeRow(customizer) {
         this.rowCustomizers.push(customizer)
         return this.repaint()
@@ -155,7 +227,7 @@ export function dataTable(dataSource) {
 
 function nav(channel, link) {
     let l = a().setClass('rap-paging ', link + '-page').title('Go to ' + link + ' page')
-    if(channel.output.get()._links?.[link])
+    if (channel.output.get()._links?.[link])
         l.onClick(set(channel.uri, channel.output.get()._links?.[link]?.href))
     else
         l.color('silver')
@@ -172,7 +244,10 @@ export function searchControls(query) {
 }
 
 export function linearizeSimpleTree(items, level = 0) {
-    return items.flatMap(item => [{level: level, node: item}, ...(item.expanded && item.children ? linearizeSimpleTree(item.children, level + 1) : [])])
+    return items.flatMap(item => [{
+        level: level,
+        node: item
+    }, ...(item.expanded && item.children ? linearizeSimpleTree(item.children, level + 1) : [])])
 }
 
 export function linearizeSimpleTreeModel(model) {
@@ -182,15 +257,15 @@ export function linearizeSimpleTreeModel(model) {
 export function linearizePageableTree(pagedTreeModel) {
     return transform(pagedTreeModel, page => {
         let l = page.content.map(item => null).flatMap(linearizePageableTree)
-        if(!page.first) l.unshift(null)
-        if(!page.last) l.push(null)
+        if (!page.first) l.unshift(null)
+        if (!page.last) l.push(null)
     })
 }
 
 export function treeColumn(name, original) {
     return new Column(name, (data, td) => {
         td.paddingLeft(data.level, 'em')
-        if(data.node.children)
+        if (data.node.children)
             td.add(expander(attach(data.node).expanded.observeChanges(trigger(original))))
         return " " + data.node.name
     })
